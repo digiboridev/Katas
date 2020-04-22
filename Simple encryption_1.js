1befd03a7fd218a0c345364ba40d4c4f3d7a694f
digibori <digiborimusik@gmail.com>
2020-04-22 20:34:38 +0300

    stealed

--- Simple encryption_1.js
+++ Simple encryption_1.js
@@ -1,52 +1,47 @@
 function encrypt(text, n) {
-    if (n <= 0) {
-        return (text)
-    } else {
-        let a = text.split('')
-        let rem = ''
-    
-        for (let i = 0; i < a.length; i += 1) {
-            rem += a.splice(i, 1)
+    if (!text || n <= 0) return text;
+    while (n--) {
+        let ans = '';
+        for (let i = 1; i < text.length; i += 2) {
+            ans += text[i];
         }
-
-        return encrypt((a.join('') + rem),--n)
+        for (let i = 0; i < text.length; i += 2) {
+            ans += text[i];
+        }
+        text = ans;
     }
+    return text;
+}
+
+function decrypt(encryptedText, n) {
+    if (!encryptedText || n <= 0) return encryptedText;
+    const ans = new Array(encryptedText.length);
+    while (n--) {
+        let j = 0;
+        for (let i = 1; i < ans.length; i += 2) {
+            ans[i] = encryptedText[j++];
+        }
+        for (let i = 0; i < ans.length; i += 2) {
+            ans[i] = encryptedText[j++];
+        }
+        encryptedText = ans.join('');
+    }
+    return encryptedText;
 }
 
 
-function decrypt(text, n) {
-    if (n <= 0) {
-        return (text)
-    } else {
-        let a = text.split('').reverse()
-        let rem = ''
-    
-        for (let i = a.length / 2, h = a.length; i > 0; i-- , h--) {
-            rem += a[i - 1]
-            rem += a[h - 1]
-        }
-        console.log(text , rem , n)
-        return decrypt(rem.toString(),--n)
-    }
-}
+console.log(encrypt("This is a test!", 0));
+console.log(encrypt("This is a test!", 1));
+console.log(encrypt("This is a test!", 2));
+console.log(encrypt("This is a test!", 3));
+console.log(encrypt("This is a test!", 4));
+console.log(encrypt("This is a test!", -1));
+console.log(encrypt("This kata is very interesting!", 1));
 
-
-
-
-
-console.log(encrypt("12345678", 0))
-console.log(encrypt("12345678asdf", 1))
-console.log(encrypt("12345678", 2))
-console.log(encrypt("12345678", 3))
-console.log(encrypt("12345678", 4))
-console.log(encrypt("12345678", -1))
-
-console.log('de')
-
-console.log(decrypt("24681357", 1))
-console.log(decrypt("hsi  etTi sats!s", 2))
-// console.log(decrypt("s eT ashi tist!", 3))
-// console.log(decrypt(" Tah itse sits!", 4))
-// console.log(decrypt("This is a test!", -1))
-// console.log(decrypt("This is a test!", 0))
-
+console.log(decrypt("This is a test!",0))
+console.log(decrypt("hsi  etTi sats!",1))
+console.log(decrypt("s eT ashi tist!",2))
+console.log(decrypt(" Tah itse sits!",3))
+console.log(decrypt("This is a test!",4))
+console.log(decrypt("This is a test!",-1))
+console.log(decrypt("hskt svr neetn!Ti aai eyitrsig",1))
